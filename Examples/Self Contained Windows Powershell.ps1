0baef4e490cc0f3bb660809277e56f8939f6cf08
Exathi <exathi.github@gmail.com>
2026-04-19 13:33:52 -0400

    move to self contained example

--- Examples/Self Contained Windows Powershell.ps1
+++ Examples/Self Contained Windows Powershell.ps1
@@ -1,5 +1,36 @@
+# Windows Powershell has an extra two delegates used in actioncommand and viewmodelbase.startasync
+# Other than these and [NoRunspaceAffinity()], the classes are the same in the self contained examples.
+# $RaiseCanExecuteChangedDelegate
+# $RemoveWorkerDelegate
+
 Add-Type -AssemblyName PresentationFramework, WindowsBase -ErrorAction Stop
-Import-Module .\CreateClassInstanceHelper.psm1
+$script:Powershell = $null
+
+function New-UnboundClassInstance ([type] $type, [object[]] $arguments = $null, [scriptblock]$definition) {
+    if ($null -eq $script:Powershell) {
+        $script:Powershell = [powershell]::Create()
+        $script:Powershell.AddScript({
+                function New-UnboundClassInstance ([type] $type, [object[]] $arguments, [scriptblock]$definition) {
+                    if ($definition) { $definition.Invoke() }
+                    [activator]::CreateInstance($type, $arguments)
+                }
+            }.Ast.GetScriptBlock()
+        ).Invoke()
+        $script:Powershell.Commands.Clear()
+    }
+
+    try {
+        if ($null -eq $arguments) { $arguments = @() }
+        $result = $script:Powershell.AddCommand('New-UnboundClassInstance').
+        AddParameter('type', $type).
+        AddParameter('arguments', $arguments).
+        AddParameter('definition', $definition).
+        Invoke()
+        return $result
+    } finally {
+        $script:Powershell.Commands.Clear()
+    }
+}
 
 # [NoRunspaceAffinity()]
 class ViewModelBase : PSCustomObject, System.ComponentModel.INotifyPropertyChanged {
